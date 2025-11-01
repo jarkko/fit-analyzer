adde4ecde92d4bfd3fa1d1d62c2c4da81b5199ca
Jarkko Laine <jarkko@jlaine.net>
2025-11-01 14:35:29 +0200

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="fitanalyzer",
-    version="0.3.2",
+    version="0.3.3",
     author="FIT Analyzer Contributors",
     description="A Python library for analyzing Garmin FIT files and calculating training metrics",
     long_description=long_description,
